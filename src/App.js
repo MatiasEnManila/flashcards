813a36a8093ecc52189bf67f30decc5f10b186f6
Matias De la Vega <mattias.delavega92@gmail.com>
2024-02-22 18:31:48 -0300

    fix code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Flashcard from './Flashcard';
 import { useState } from 'react';
 
 function App(props) {
+  const [button, setButton] = useState(false);
 
-  const [button, setButton] = useState(false);  
   const [frontFaceFlashcard, setFrontFaceFlashcard] = useState(''); 
-
   const [BackFaceFlashcard, setBackFaceFlashcard] = useState('');
+  
   const [flashcards, setFlashcards] = useState([]);
   
 
@@ -16,34 +16,20 @@ function App(props) {
     setFlashcards(flashcards.filter((flashcard, index) => index != indexToDelete));
   }
 
-
-  const isValidUrl = urlString=> {
-    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-  '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-return !!urlPattern.test(urlString);
-}
-
-  const handleChangeFrontFace = (event) => { //Cara delantera de la flashcard
-      setFrontFaceFlashcard(event.target.value); //guarda el input del campo en fronFaceFlashcard}
+  const handleChangeFrontFace = (event) => {
+    setFrontFaceFlashcard(event.target.value); // Guarda el input del campo en fronFaceFlashcard
   } 
   
   const handleChangeBackFace = (event) => {
-    if (isValidUrl(event.target.value)) {
-    } else {
-      setBackFaceFlashcard(event.target.value);
-    }
+    setBackFaceFlashcard(event.target.value);
   }
 
   
-  const handleClick = () => { // Guarda en flashcards el input puesto en forma de objeto
+  const createFlashcard = () => { // Guarda en flashcards el input puesto en forma de objeto
     setFlashcards([...flashcards, {frontFace: frontFaceFlashcard, backFace: BackFaceFlashcard}]);
   }
   
-  const updateButton = () => { //
+  const updateButton = () => {
     setButton(!button);
   }
 
@@ -57,19 +43,16 @@ return !!urlPattern.test(urlString);
             id="frontFaceFlashcard" 
             name="frontFaceFlashcard"
             onChange={handleChangeFrontFace}
-            value={frontFaceFlashcard}>
-          </input>
-
+          />
           <label htmlFor='BackFaceFlashcard'>Face backward</label>
           <input 
             type="text"
             id="BackFaceFlashcard" 
             name="BackFaceFlashcard"
             onChange={handleChangeBackFace}
-            value={BackFaceFlashcard}>
-          </input>
+          />
         </form>
-        <button onClick={handleClick}>Submit</button>
+        <button onClick={createFlashcard}>Submit</button>
         <button onClick={() => setButton(!button)}>Go back</button>
       </div>
 
@@ -79,20 +62,13 @@ return !!urlPattern.test(urlString);
     } else { // HOME  
       return (
         <div className="App">
-
-          {/* <div>
-           <button onClick={() => setIsTrue(!isTrue)}>Button</button>
-           {isTrue ? console.log("true") : console.log("fake")}
-          </div>   */}
-          <button className='button' onClick={() => updateButton(!button)}>Create new deck</button>
+          <button className='button' onClick={() => updateButton(!button)}>Create new flashcard</button>
           <div>
           {flashcards.length > 0 && flashcards.map((flashcard, i) => <Flashcard frontFace={flashcard.frontFace} backFace={flashcard.backFace} deleteFlashcard={() => deleteFlashcard(i)}/>)}
           </div>
         </div> 
     );
   }
-  {/* <button className='button'>Button 2</button>
-    <button className='button'>Button 3</button> */}
 }
 
 
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,57 +1,43 @@
-// import { isVisible } from '@testing-library/user-event/dist/utils';
 import '../src/chinese-characters/stylesheets/Flashcard.css';
 import { useState } from 'react';
 
 function Flashcard({frontFace, backFace, deleteFlashcard}) {
-    // TODO click event handler para borrar la flashcards en la cual uno clickea, usar metodo filter para deletearla?
-    // TODO  Render: Actulizar el DOM? el html
-    // TODO  Que es lo que quiero filtrar para poder eliminar?
-    const [faceForwardFlashcard, setfaceForwardFlashcard] = useState(true);
-    // const [deleteFlashcard, setDeleteFlashcard]  = useState(frontFace, backFace);
-
-    // const onDelete = () => {
-    //     setDeleteFlashcard(deleteFlashcard.filter(flashcard => flashcard))
-    // }
+  const [faceForwardFlashcard, setfaceForwardFlashcard] = useState(true);
+  
+  const flipcard = (newValue) => {
+    setfaceForwardFlashcard(newValue);
+  }
+  
+  const isValidUrl = urlString=> {
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+    '(\\#[-a-z\\d_]*)?'+
+    '\\.(jpg|jpeg|png|webp|avif|gif|svg)$','i'); // validate fragment locator
+    return !!urlPattern.test(urlString);
+  }
+  
+  if (faceForwardFlashcard) {
+    return (
+      <div className='flashcard front-flash'>
+        <div onClick={() => flipcard(!faceForwardFlashcard)}>{isValidUrl(frontFace) ? <img src={(frontFace)}  alt="picture"/> : frontFace}</div>
+        <div>
+          <button type="button" onClick={deleteFlashcard}>Borrar flashcard</button> 
+        </div>
+      </div>
+    )
+  } else {
+    return ( 
+      <div className='flashcard back-flash'>
+        <div onClick={() => flipcard(!faceForwardFlashcard)}>{isValidUrl(backFace) ? <img src={(backFace)}  alt="picture"/> : backFace}</div>
+        <div>
+          <button type="button" onClick={deleteFlashcard}>Borrar flashcard</button> 
+        </div>
+      </div>
+    )
+  }
+ };
     
-    const flipcard = (newValue) => {
-        setfaceForwardFlashcard(newValue);
-    }
-        
-    const isValidUrl = urlString=> {
-        var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-        '(\\#[-a-z\\d_]*)?'+
-        '\\.(jpg|jpeg|png|webp|avif|gif|svg)$','i'); // validate fragment locator
-        return !!urlPattern.test(urlString);
-    }
-
-    if (faceForwardFlashcard) {
-        return (
-            <div className='flashcard front-flash'>
-                <div onClick={() => flipcard(!faceForwardFlashcard)}>{isValidUrl(frontFace) ? <img src={(frontFace)}  alt="picture"/> : frontFace}</div>
-                <div>
-                <button type="button" onClick={deleteFlashcard}>Borrar flashcard</button> 
-                    {/* Filtrar todo lo que no sea una string 
-                      button tiene que devolver un return? Return sobre return? 
-                    */}
-                </div>
-            </div>
-        )
-    } else {
-        return ( 
-            <div className='flashcard back-flash'>
-                <div onClick={() => flipcard(!faceForwardFlashcard)}>{isValidUrl(backFace) ? <img src={(backFace)}  alt="picture"/> : backFace}</div>
-                <div>
-                    {/* <button onClick={remove}>Delete</button> */}
-                </div>
-            </div>
-        )
-      }
-    };
-
-
-{/* // <img src={ChineseCharacter} className="chinese-character" alt="kanji"  */}
 export default Flashcard;
